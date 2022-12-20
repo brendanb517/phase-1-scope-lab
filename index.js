7786e34c71059d79b5e0b6254c03facc1e509535
Brendan Baird <brendan.b517@gmail.com>
2022-12-19 21:16:38 -0500

    commit tree ba8b22722b8fec5930b79cf44586b6607ec0aa2d

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = "bob";
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+function setBestCustomer(){
+    window.bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "definitely bob";
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "not bob after all";
+}
